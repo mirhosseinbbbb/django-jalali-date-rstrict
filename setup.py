9f96cc4d4492821540501995ef1fe02d5af97dae
mirhosseinbbbb <basati.mie@gmail.com>
2025-08-16 09:33:59 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read_me(filename):
 
 
 setup(
-    name='django-jalali-date',
+    name='django-jalali-date-rstrict',
     version='2.0.0',
     python_requires='>=3',
     packages=find_packages(),
@@ -18,7 +18,7 @@ setup(
         'Jalali Date support for user interface. Easy conversion of DateTimeFiled to JalaliDateTimeField within the admin site, views, forms and templates.'
     ),
     url='http://github.com/a-roomana/django-jalali-date',
-    download_url='https://pypi.python.org/pypi/django-jalali-date/',
+    download_url='https://pypi.python.org/pypi/django-jalali-date-rstrict/',
     author='Arman Roomana',
     author_email='roomana.arman@gmail.com',
     keywords="django jalali date",
